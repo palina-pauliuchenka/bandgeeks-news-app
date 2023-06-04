39f4e41a4344d825826602d8b7c19feee007db89
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-06-04 00:20:08 -0400

    Initial working Sign-In page. Requires correct db table name. Requires extensive testing.

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,52 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useState } from 'react'
+import { navigate } from '@redwoodjs/web';
+
 
 const LoginPage = () => {
+
+const [userId, setUserId] = useState('');
+const [password, setPassword] = useState('');
+const [error, setError] = useState(' ');
+
+const authLogic = () => {
+
+   db./* insert database table name */.findFirst({where: {userid, password,},}).then((/* insert database table name */) => {
+      if (/* insert database table name which also acts as the T/F value */)
+      {
+        navigate('/landing');
+      }
+      else
+      {
+        setError("Incorrect User Id or Password. Try again.");
+      }
+    }).catch((error) => {
+      setError("Something went wrong. Please try again.");
+    });
+};
+
   return (
     <>
-      <MetaTags title="Login" description="Login page" />
-
-      <h1>LoginPage</h1>
+      <MetaTags title="Login" description="Login" />
+      <center>
+      <h1>Login Page</h1>
+    <form>
       <p>
-        Find me in <code>./web/src/pages/LoginPage/LoginPage.js</code>
+      <label htmlFor="userid">User Id:</label><br></br>
+      <input type="text" id="userid" name="userid" required />
       </p>
       <p>
-        My default route is named <code>login</code>, link to me with `
-        <Link to={routes.login()}>Login</Link>`
+      <label htmlFor="password">Password:</label><br></br>
+      <input type="password" id="password" name="password" required />
       </p>
+      <button type="submit" onClick={authLogic}>Login</button>
+      </form>
+      <p>
+        Don't have an account?
+        <Link to={routes.signup()}> Sign Up</Link>
+      </p>
+      </center>
     </>
   )
 }
