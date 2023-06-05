5b189379294d5fe99fc3ac10a540a2b5e62b9db7
snaramirez872 <seanramirez818@gmail.com>
2023-06-04 21:58:55 -0400

    building the signup page part 1

--- web/src/pages/SignupPage/SignupPage.js
+++ web/src/pages/SignupPage/SignupPage.js
@@ -1,7 +1,55 @@
-import { Link, routes } from '@redwoodjs/router'
+import { Link, navigate, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { toast, Toaster } from '@redwoodjs/web/toast'
+
+// For Form Usage (from Redwood Tutorial)
+import {
+  Form,
+  Label,
+  TextField,
+  PasswordField,
+  FieldError,
+  Submit,
+} from '@redwoodjs/forms'
+
+import { useAuth } from 'src/auth'
+import { useEffect } from 'react'
+
 
 const SignupPage = () => {
+
+  const { isAuthenticated, signUp } = useAuth()
+  /*
+  // Uncomment this block when homepage is made
+  // Should redirect to home after signing up
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate(routes.home())
+    }
+  }, [isAuthenticated])
+  */
+
+  const usernameRef = useRef(null)
+  useEffect(() => {
+    usernameRef.current?.focus()
+  }, [])
+
+  const onSubmit = async (data) => {
+    const response = await signUp({
+      username: data.username,
+      password: data.password,
+    })
+
+    if (response.message) {
+      toast(response.message)
+    } else if (response.error) {
+      toast.error(response.error)
+    } else {
+      // user is signed in automatically
+      toast.success('Welcome!')
+    }
+  }
+
   return (
     <>
       <MetaTags title="Signup" description="Signup page" />
