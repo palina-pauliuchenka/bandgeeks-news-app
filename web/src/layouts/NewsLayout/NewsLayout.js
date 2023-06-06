8206acf8fade19a48f676efa4cea24dd70b8b980
Palina Pauliuchenka <ppauliuchenka02@gmail.com>
2023-06-06 15:03:33 -0400

    very baaasic landing page

---
+++ web/src/layouts/NewsLayout/NewsLayout.js
@@ -0,0 +1,27 @@
+import { Link, routes } from '@redwoodjs/router'
+
+const NewsLayout = ({ children }) => {
+  return (
+    <>
+      <header>
+        <h1>Band Geeks</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to={routes.home()}>Home</Link>
+            </li>
+            <li>
+              <Link to={routes.about()}>About</Link>
+            </li>
+            <li>
+              <Link to={routes.meme()}>Meme</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>{children}</main>
+    </>
+  )
+}
+
+export default NewsLayout
