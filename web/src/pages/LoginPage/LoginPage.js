3d17af05770afda6682643ef81136d69905dbd0f
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-06-05 23:14:36 -0400

    Working Login page with db name, and updated visuals. Needs testing before sprint demo.

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -12,39 +12,41 @@ const [error, setError] = useState(' ');
 
 const authLogic = () => {
 
-   db./* insert database table name */.findFirst({where: {userid, password,},}).then((/* insert database table name */) => {
-      if (/* insert database table name which also acts as the T/F value */)
-      {
-        navigate('/landing');
-      }
-      else
-      {
-        setError("Incorrect User Id or Password. Try again.");
-      }
-    }).catch((error) => {
-      setError("Something went wrong. Please try again.");
-    });
-};
+  db.User.findFirst({where: {userid, password,},}).then((User) => {
+    if (User)
+    {
+      navigate('/landing');
+    }
+    else
+    {
+      setError("Incorrect User Id or Password. Try again.");
+    }
+  }).catch((error) => {
+    setError("Something went wrong. Please try again.");
+  });
+
+}
 
   return (
     <>
       <MetaTags title="Login" description="Login" />
+      <br></br><br></br>
       <center>
-      <h1>Login Page</h1>
+      <h1 style={{ fontSize: '50px' }}>Login</h1>
     <form>
       <p>
-      <label htmlFor="userid">User Id:</label><br></br>
+      <label htmlFor="userid" style={{ fontSize: '20px', marginLeft: '-90px' }}>Username:</label><br></br>
       <input type="text" id="userid" name="userid" required />
       </p>
       <p>
-      <label htmlFor="password">Password:</label><br></br>
+      <label htmlFor="password" style={{ fontSize: '20px', marginLeft: '-90px' }}>Password:</label><br></br>
       <input type="password" id="password" name="password" required />
       </p>
-      <button type="submit" onClick={authLogic}>Login</button>
+      <button type="submit" onClick={authLogic} style={{ fontSize: '18px', backgroundColor: 'green', color: 'white', fontWeight: 'bold'}}>Login</button>
       </form>
-      <p>
-        Don't have an account?
-        <Link to={routes.signup()}> Sign Up</Link>
+      <p>_________________________</p>
+      <p style={{ fontSize: '20px' }}>
+        Don't have an account? <Link to={routes.signup()} >Create account</Link>
       </p>
       </center>
     </>
