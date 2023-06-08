db4abe8a5141945b0742fc0717708016bc5af59f
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-06-08 00:37:09 -0400

    Updated Login.js with removed styling and error handling upon incorrect password

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,19 +1,20 @@
-import { useState } from 'react'
-
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
+import { useState } from 'react'
 import { navigate } from '@redwoodjs/web'
 
 const LoginPage = () => {
-  const [userId, setUserId] = useState('')
+  const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
-  const [error, setError] = useState(' ')
+  const [error, setError] = useState('')
 
-  const authLogic = () => {
-    db.User.findFirst({ where: { userid, password } })
-      .then((User) => {
-        if (User) {
-          navigate('/landing')
+  const authLogic = (event) => {
+    event.preventDefault()
+
+    db.User.findFirst({ where: { username, password } })
+      .then((user) => {
+        if (user) {
+          navigate('/')
         } else {
           setError('Incorrect User Id or Password. Try again.')
         }
@@ -24,53 +25,53 @@ const LoginPage = () => {
   }
 
   return (
-    <>
-      <MetaTags title="Login" description="Login" />
-      <br></br>
-      <br></br>
-      <center>
-        <h1 style={{ fontSize: '50px' }}>Login</h1>
-        <form>
-          <p>
-            <label
-              htmlFor="userid"
-              style={{ fontSize: '20px', marginLeft: '-90px' }}
-            >
-              Username:
-            </label>
-            <br></br>
-            <input type="text" id="userid" name="userid" required />
-          </p>
-          <p>
-            <label
-              htmlFor="password"
-              style={{ fontSize: '20px', marginLeft: '-90px' }}
-            >
-              Password:
-            </label>
-            <br></br>
-            <input type="password" id="password" name="password" required />
-          </p>
-          <button
-            type="submit"
-            onClick={authLogic}
-            style={{
-              fontSize: '18px',
-              backgroundColor: 'green',
-              color: 'white',
-              fontWeight: 'bold',
-            }}
-          >
-            Login
-          </button>
-        </form>
-        <p>_________________________</p>
-        <p style={{ fontSize: '20px' }}>
-          Don't have an account?{' '}
-          <Link to={routes.signup()}>Create account</Link>
-        </p>
-      </center>
-    </>
+    <main className="rw-main">
+      <div className="rw-scaffold rw-login-container">
+        <div className="rw-segment">
+          <header className="rw-segment-header">
+            <h1 className="rw-heading rw-heading-secondary">Log In</h1>
+          </header>
+
+          <div className="rw-segment-main">
+            <div className="rw-form-wrapper">
+              {error && <p>{error}</p>}
+
+              <form onSubmit={authLogic}>
+                <p>
+                  <label htmlFor="username">Username</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </p>
+                <p>
+                  <label htmlFor="password">Password</label>
+                  <br />
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </p>
+                <button type="submit">Login</button>
+              </form>
+              <p>
+                Don't have an account?{' '}
+                <Link to={routes.signup()}>Create account</Link>
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
