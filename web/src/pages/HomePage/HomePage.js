bee828265bb200bd5e3905849003e9ea12646180
Palina Pauliuchenka <ppauliuchenka02@gmail.com>
2023-06-08 17:36:53 -0400

    Update HomePage.js

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -6,6 +6,21 @@ const HomePage = () => {
       <MetaTags title="Home" description="Home page" />
 
       <main>
+
+        <div className="relative w-full h-[calc(100vh-100px)]">
+          <div className="absolute transform top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+            <figure className="snip1113 red font-gruppo shadow-lg block p-6">
+              <img src="https://static1.srcdn.com/wordpress/wp-content/uploads/2020/08/Animal-Crossing-New-Horizons-Spongebob-Squarepants-Band-Geeks.jpg" alt="band geeks" />
+              <figcaption>
+                <h3 className="tracking-widest">Band <span>Geeks</span></h3>
+                <h4>
+                  Team
+                </h4>
+              </figcaption>
+            </figure>
+          </div>
+        </div>
+
         <h2 className="text-center mt-12 text-3xl font-bold">Project Information</h2>
         <div className="flex justify-center">
           <p className="mt-12 leading-loose font-mono">
