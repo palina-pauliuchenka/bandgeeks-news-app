e7ab81d4b908424d3b9798bb69adf0ec11d0eeac
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-06-07 20:37:40 -0400

    Updated final login page with new template and error handling for incorrect password as of 6/7/23

--- web/src/pages/LoginPage/LoginPage.js
+++ web/src/pages/LoginPage/LoginPage.js
@@ -1,66 +1,78 @@
 import { Link, routes } from '@redwoodjs/router'
 import { MetaTags } from '@redwoodjs/web'
 import { useState } from 'react'
-import { navigate } from '@redwoodjs/web';
-
+import { navigate } from '@redwoodjs/web'
 
 const LoginPage = () => {
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [error, setError] = useState('')
 
-const [userId, setUserId] = useState('');
-const [password, setPassword] = useState('');
-const [error, setError] = useState('');
+  const authLogic = (event) => {
+    event.preventDefault()
 
-const authLogic = () => {
-  db.User.findFirst({where: {username, password,},}).then((User) => {
-    if (User)
-    {
-      navigate('/');
-    }
-    else
-    {
-      setError("Incorrect User Id or Password. Try again.");
-    }
-  }).catch((error) => {
-    setError("Something went wrong. Please try again.");
-  });
-}
+    db.User.findFirst({ where: { username, password } })
+      .then((user) => {
+        if (user) {
+          navigate('/')
+        } else {
+          setError('Incorrect User Id or Password. Try again.')
+        }
+      })
+      .catch((error) => {
+        setError('Something went wrong. Please try again.')
+      })
+  }
 
-return (
-  <main className="rw-main">
-    <div className="rw-scaffold rw-login-container">
-      <div className="rw-segment">
-        <header className="rw-segment-header">
-          <h1 className="rw-heading rw-heading-secondary">Log In</h1>
-        </header>
+  return (
+    <main className="rw-main">
+      <div className="rw-scaffold rw-login-container">
+        <div className="rw-segment">
+          <header className="rw-segment-header">
+            <h1 className="rw-heading rw-heading-secondary">Log In</h1>
+          </header>
 
-        <div className="rw-segment-main">
-          <div className="rw-form-wrapper">
-            <form>
+          <div className="rw-segment-main">
+            <div className="rw-form-wrapper">
+              {error && <p>{error}</p>}
+
+              <form onSubmit={authLogic}>
+                <p>
+                  <label htmlFor="username">Username</label>
+                  <br />
+                  <input
+                    type="text"
+                    id="username"
+                    name="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                    required
+                  />
+                </p>
+                <p>
+                  <label htmlFor="password">Password</label>
+                  <br />
+                  <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    required
+                  />
+                </p>
+                <button type="submit">Login</button>
+              </form>
               <p>
-                <label htmlFor="userid">Username</label><br></br>
-                <input type="text" id="userid" name="userid" required />
+                Don't have an account?{' '}
+                <Link to={routes.signup()}>Create account</Link>
               </p>
-              <p>
-                <label htmlFor="password">Password</label><br></br>
-                <input type="password" id="password" name="password" required />
-              </p>
-              <button
-                type="submit"
-                onClick={authLogic}
-              >
-                Login
-              </button>
-            </form>
-            <p>
-              Don't have an account?{' '}
-              <Link to={routes.signup()}>Create account</Link>
-            </p>
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  </main>
-)
+    </main>
+  )
 }
 
 export default LoginPage
