113e873ffdec7b9f3e710b82013bcbca8e0f40a9
Carlos Bonilla <cdb24@njit.edu>
2023-06-20 14:51:12 -0400

    migrate prisma database

---
+++ api/db/migrations/20230620183228_news_categories/migration.sql
@@ -0,0 +1,20 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "hashedPassword" TEXT NOT NULL,
+    "salt" TEXT NOT NULL,
+    "resetToken" TEXT,
+    "resetTokenExpiresAt" DATETIME,
+    "newsGeneral" BOOLEAN NOT NULL DEFAULT false,
+    "newsBusiness" BOOLEAN NOT NULL DEFAULT false,
+    "newsEntertainment" BOOLEAN NOT NULL DEFAULT false,
+    "newsHealth" BOOLEAN NOT NULL DEFAULT false,
+    "newsScience" BOOLEAN NOT NULL DEFAULT false,
+    "newsSports" BOOLEAN NOT NULL DEFAULT false,
+    "newsTechnology" BOOLEAN NOT NULL DEFAULT false
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
