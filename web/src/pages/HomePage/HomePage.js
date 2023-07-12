8e44b0a0808d977fe1c373a843c2c702a7afdb1f
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-07-12 17:47:22 -0400

    Edited "re-search" link and now "x" takes you back to home

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -142,7 +142,7 @@ const HomePage = () => {
           <button className = "text-blue-500 hover:text-blue-700 underline"
           onClick={()=> window.history.back()}
           >
-            <p className={'my-6 font-gruppo text-2xl font-black capitalize'}>Cancel gibberish warning message and search again</p>
+            <p className={'my-6 font-gruppo text-2xl font-black capitalize'}>Edit search and try again</p>
           </button>
         }
         <div
@@ -163,6 +163,7 @@ const HomePage = () => {
               role="button"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
+              onClick={() => window.location.href = '/'}
             >
               <title>Close</title>
               <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
