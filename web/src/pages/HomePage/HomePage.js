1a797186bc3ebbf9921115f15c1f036c1d981104
Samridhya Mukherjee <134455646+SamridhyaM@users.noreply.github.com>
2023-07-13 13:37:20 -0400

    User now stays on the same page even after clicking "x". Will create the new search results page

--- web/src/pages/HomePage/HomePage.js
+++ web/src/pages/HomePage/HomePage.js
@@ -140,6 +140,7 @@ const HomePage = () => {
 
       <div className={'mt-12 w-full text-center'}>
         <div
+        id = "alert"
           className="relative mx-auto max-w-3xl rounded border border-red-400 bg-red-100 px-4 py-3 text-red-700"
           role="alert"
         >
@@ -149,13 +150,20 @@ const HomePage = () => {
           <p>
             Unfortunately we were not able to find anything regarding your search!
           </p>
-          <span className="absolute bottom-0 right-0 top-0 px-4 py-3">
+          <span className="absolute bottom-0 right-0 top-0 px-4 py-3"
+          onClick={() => {
+            const alertElement = document.getElementById('alert');
+            if (alertElement) {
+              alertElement.style.display = 'none';
+            }
+          }}
+          >
             <svg
               className="h-6 w-6 fill-current text-red-500"
               role="button"
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 20 20"
-              onClick={() => window.history.back()}
+              //onClick={() => /* something happens here */}
             >
               <title>Close</title>
               <path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z" />
